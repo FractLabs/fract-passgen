fc802a15d4f335b6310f62f982f9803de6450fe9
EuSouUmDoge <eusouumdoge@gmail.com>
2025-06-29 19:44:58 -0300

    code cleanup & added a character sistem

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@
 //use std::time::Duration;
 //use rand::*;
 use rand::random_range;
+use std::thread;
+use std::time::Duration;
 use std::io::{self, BufRead, Write};
 //use std::fs::File;
 use std::fs::{File, OpenOptions};
@@ -10,52 +12,12 @@ use std::fs::{File, OpenOptions};
 
 
 fn main() {
-
-    let filep = "numbers.txt";
-    let filer = File::open(filep).expect("error: open");
-    let reader =  io::BufReader::new(filer);
-    let mut rw_in = String::new();
-    println!("Read or Write? (R/W) ");
-    io::stdin()
-        .read_line(&mut rw_in)
-        .expect("Fail in read");
-
-    let mut num_file = OpenOptions::new()
-    .append(true)
-    .create(true)
-    .open(filep)
-    .expect("fail");
-
-    let mut times = 0;
-
-    if rw_in.trim() == "W" {
-
     loop {
-//    let ims = 0;
-    let ranber: i32 = random_range(1..=1000000);
-//    let tofind: i32 = 1;
-    times += 1;
+    let rancar:char = random_range('a'..='z') as char;
+    let ranber:i32 = random_range(0..=99999);
 
-//    thread::sleep(Duration::from_millis(ims));
-//    println!("{}", &ranber);
-//    let content = String::from("{}", &ranber);
-//    num_file.write(content).expect("write fail");
-    writeln!(num_file, "{}", &ranber).expect("fail in writing");
-    if times == 1000000000 {
-        break;
-    }
-    println!("{}", times)
-    }
-} else if rw_in.trim() == "R" {
-    for linha in reader.lines() {
-        let linha = linha.expect("Error");
-        println!("{}", linha)
+    println!("{} and {}", ranber, rancar);
+    thread::sleep(Duration::from_secs(1));
     }
 } 
     
-
-
-}
-
-
-//eq(&ranber, tofind)
