0c7f28a4d04634a34f18568bd37781c3e4ca2dd5
EuSouUmDoge <eusouumdoge@gmail.com>
2025-06-30 11:14:29 -0300

    A few code changes

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,27 @@
 //use std::time::Duration;
 //use rand::*;
 use rand::random_range;
+use std::io::{self, BufRead, Write};
 use std::thread;
 use std::time::Duration;
-use std::io::{self, BufRead, Write};
 //use std::fs::File;
 use std::fs::{File, OpenOptions};
 
-
-
 fn main() {
-    loop {
-    let rancar:char = random_range('a'..='z') as char;
-    let ranber:i32 = random_range(0..=99999);
+    let mut times: i32 = 0;
 
-    println!("{} and {}", ranber, rancar);
-    thread::sleep(Duration::from_secs(1));
+    //println!("");
+    loop {
+        let rancar: char = random_range('a'..='z') as char;
+        let ranber: i32 = random_range(0..=99999);
+        times += 1;
+
+        print!("{}{}", ranber, rancar);
+        std::io::Write::flush(&mut std::io::stdout()).unwrap();
+        //        thread::sleep(Duration::from_secs(1));
+
+        if times == 3 {
+            break;
+        }
     }
-} 
-    
+}
