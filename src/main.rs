e437a5981c1cd5503b93b00b90a59b9a65a53330
EuSouUmDoge <eusouumdoge@gmail.com>
2025-06-30 11:38:48 -0300

    added actually functional password generation with custom length

--- src/main.rs
+++ src/main.rs
@@ -2,26 +2,38 @@
 //use std::time::Duration;
 //use rand::*;
 use rand::random_range;
-use std::io::{self, BufRead, Write};
-use std::thread;
-use std::time::Duration;
+//use std::io::{self, BufRead, Write};
+//use std::thread;
+//use std::time::Duration;
 //use std::fs::File;
-use std::fs::{File, OpenOptions};
+//use std::fs::{File, OpenOptions};
 
 fn main() {
     let mut times: i32 = 0;
+    let mut final_pass = String::new();
+    let mut pass_length = 7;
 
     //println!("");
     loop {
-        let rancar: char = random_range('a'..='z') as char;
-        let ranber: i32 = random_range(0..=99999);
-        times += 1;
+        let ranw:i32 = random_range(0..=1);
+        
 
-        print!("{}{}", ranber, rancar);
-        std::io::Write::flush(&mut std::io::stdout()).unwrap();
+        times += 1;
+        if ranw == 0 {
+            let ranber: i32 = random_range(0..=9);
+            final_pass.push_str(&ranber.to_string());
+        } else if ranw == 1 {
+            let rancar: char = random_range('a'..='z') as char;
+            final_pass.push(rancar);
+        }
+
+//        print!("{}{}", ranber, rancar);
+//        println!("{}", ranw);
+//        std::io::Write::flush(&mut std::io::stdout()).unwrap();
         //        thread::sleep(Duration::from_secs(1));
 
-        if times == 3 {
+        if times >= pass_length {
+            println!("{}", final_pass);
             break;
         }
     }
