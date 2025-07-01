76e204d9f0d38d18ecc5f00c4bd6ad20a2030500
EuSouUmDoge <eusouumdoge@gmail.com>
2025-07-01 15:06:24 -0300

    Added uppercase characters

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ use rand::random_range;
 //use std::fs;
 
 
+
 fn main() {
     let spechar: [char; 6] = ['!', '@', '#', '$', '%', '&'];
     let mut times: i32 = 0;
@@ -16,7 +17,7 @@ fn main() {
 // Let it mut when implementing pass length input
     //println!("");
     loop {
-        let ranw:i32 = random_range(0..=2);
+        let ranw:i32 = random_range(0..=3);
         
 
         times += 1;
@@ -30,6 +31,9 @@ fn main() {
             let rannumspe = random_range(0..6);
             final_pass.push(spechar[rannumspe]);
             
+        } else if ranw == 3 {
+            let rancarm: char = random_range('a'..='z');
+            final_pass.push(rancarm.to_uppercase().next().unwrap());
         }
 
 //        std::io::Write::flush(&mut std::io::stdout()).unwrap();
