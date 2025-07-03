bd90aafcfa37b23278cacd0d9c5f0b81aa6cbc4b
EuSouUmDoge <eusouumdoge@gmail.com>
2025-07-02 22:31:05 -0300

    Fixed bug in which if there some of the bools in the function were false it would just skip

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,10 @@ fn main() {
 
     let pass_length: i32 = 9;
 
-    let uppercase_letters: bool = true;
-    let letters: bool = true;
-    let numbers: bool = true;
-    let special_characters: bool = true;
+    let uppercase_letters: bool = false;
+    let letters: bool = false;
+    let numbers: bool = false;
+    let special_characters: bool = false;
 
     let final_pass = passgen::passgen(pass_length, uppercase_letters, letters, numbers, special_characters);
 
