5162d16a3ad3a5a3a7623aef561bfe5cd224ccfa
EuSouUmDoge <eusouumdoge@gmail.com>
2025-07-03 14:02:48 -0300

    Preparing for UI

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 mod passgen;
 
 fn main() {
-
+    
     let pass_length: i32 = 9;
 
-    let uppercase_letters: bool = false;
-    let letters: bool = false;
-    let numbers: bool = false;
-    let special_characters: bool = false;
+    let uppercase_letters: bool = true;
+    let letters: bool = true;
+    let numbers: bool = true;
+    let special_characters: bool = true;
 
     let final_pass = passgen::passgen(pass_length, uppercase_letters, letters, numbers, special_characters);
 
